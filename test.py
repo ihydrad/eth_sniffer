b7cfeeb751d10e64c24f7bcd582e0ad248cd204a
ihydrad <S-tehnikum@rambler.ru>
2022-08-03 23:06:40 +0300

    111

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 import socket
 import struct
+import subprocess
+import threading
+import argparse
+from ipaddress import ip_address
+from time import sleep
 
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+active = True
 
 def get_ip(text):
     hex_data = text.hex()
     return f"{int(hex_data[:2], 16)}.{int(hex_data[2:4], 16)}.{int(hex_data[4:6], 16)}.{int(hex_data[6:8], 16)}"
-
 
 def ipv4_parser(raw_data):
     ip_pocket = {}
@@ -27,7 +31,6 @@ def ethernet_parser(raw_data):
     eth_frame["data"] = raw_data[14:]
     return eth_frame
 
-
 def main():
     s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
     while True:
@@ -41,6 +44,39 @@ def main():
             print(f'{ipv4["src"]} -> {ipv4["dst"]} [{ipv4["proto"]}]')
             #print(f'Version:{ipv4["version"]}, Header Length:{ipv4["header_length"]}, TTL:{ipv4["ttl"]}\n')
             
+def catch_frame(addr):
+    s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+    while active:
+        raw_data, iface = s.recvfrom(65535)
+        if iface[0] == 'lo':
+            continue
+        frame = ethernet_parser(raw_data)        
+        if frame["proto"] == 8:
+            ipv4 = ipv4_parser(frame["data"])
+            if ipv4["dst"] == addr:
+                return print(f"{addr} catched on {iface[0]}") 
+
+def generate(addr, method):
+    if method == "icmp":
+        subprocess.call(f"ping -c 1 {addr} > null", shell=True)
+    else:
+        raise NotImplementedError
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("addr", help="destination")
+    parser.add_argument("-m", dest="method", help="destination", default="icmp")
+    args = parser.parse_args()
+    try:
+        ip_address(args.addr)
+    except ValueError:
+        print("Input correct ip-address!")
+    catcher = threading.Thread(target=catch_frame, args=(args.addr,))
+    catcher.start()
+    try:
+        generate(args.addr, args.method)
+        sleep(5)
+    except NotImplementedError:
+        active = False
+        print("Not Implemented")
+    
