59c571ae67372ace8bf6113df3568ec48589e9a5
ihydrad <S-tehnikum@rambler.ru>
2022-08-04 10:10:28 +0300

    111

--- catcher.py
+++ catcher.py
@@ -7,11 +7,23 @@ from ipaddress import ip_address
 from time import sleep
 import sys
 
+ETH_P_ALL = 3
+ETH_P_IP = 0x800
+ETH_P_ARP = 0x806
 active = True
 
 def get_ip(text):
     hex_data = text.hex()
     return f"{int(hex_data[:2], 16)}.{int(hex_data[2:4], 16)}.{int(hex_data[4:6], 16)}.{int(hex_data[6:8], 16)}"
+
+def ethernet_parser(raw_data):
+    eth_frame = {}
+    dest, src, prototype = struct.unpack('! 6s 6s H', raw_data[:14])
+    eth_frame["dst"] = dest.hex()
+    eth_frame["src"] = src.hex()
+    eth_frame["proto"] = prototype #socket.htons(prototype)
+    eth_frame["data"] = raw_data[14:]
+    return eth_frame
 
 def ipv4_parser(raw_data):
     ip_pocket = {}
@@ -23,14 +35,8 @@ def ipv4_parser(raw_data):
     ip_pocket["data"] = raw_data[ip_pocket["header_length"]:]
     return ip_pocket
 
-def ethernet_parser(raw_data):
-    eth_frame = {}
-    dest, src, prototype = struct.unpack('! 6s 6s H', raw_data[:14])
-    eth_frame["dst"] = dest.hex()
-    eth_frame["src"] = src.hex()
-    eth_frame["proto"] = socket.htons(prototype)
-    eth_frame["data"] = raw_data[14:]
-    return eth_frame
+def arp_parser(raw_data):
+    pass
 
 def main():
     s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
@@ -39,22 +45,27 @@ def main():
         if addr[0] == 'lo':
             continue
         frame = ethernet_parser(raw_data)
-        print(f'{frame["src"]} -> {frame["dst"]} [{frame["proto"]}]        {addr[0]}') 
-        if frame["proto"] == 8:
-            ipv4 = ipv4_parser(frame["data"])
-            print(f'{ipv4["src"]} -> {ipv4["dst"]} [{ipv4["proto"]}]')
-            #print(f'Version:{ipv4["version"]}, Header Length:{ipv4["header_length"]}, TTL:{ipv4["ttl"]}\n')
+        if frame["proto"] == ETH_P_ARP:
+            print(f'{frame["src"]} -> {frame["dst"]} [{hex(frame["proto"])}]        {addr[0]}') 
+        # if frame["proto"] == 8:
+        #     ipv4 = ipv4_parser(frame["data"])
+        #     print(f'{ipv4["src"]} -> {ipv4["dst"]} [{ipv4["proto"]}]')
+        #     #print(f'Version:{ipv4["version"]}, Header Length:{ipv4["header_length"]}, TTL:{ipv4["ttl"]}\n')
             
 def catch_frame(addr):
-    s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+    s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(ETH_P_ALL))
     while active:
         raw_data, iface = s.recvfrom(65535)
         if iface[0] == 'lo':
             continue
         frame = ethernet_parser(raw_data)        
-        if frame["proto"] == 8:
+        if frame["proto"] == ETH_P_IP:
             ipv4 = ipv4_parser(frame["data"])
             if ipv4["dst"] == addr:
+                return print(f"Pocket with dst addr {addr} catched on {iface[0]}") 
+        if frame["proto"] == ETH_P_ARP:
+            arp = arp_parser(frame["data"])
+            if arp["dst"] == addr:
                 return print(f"Pocket with dst addr {addr} catched on {iface[0]}") 
 
 def generate(addr, method):
